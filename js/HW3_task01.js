48c51d279c6bb335847f5b0efd6c79531b84c502
Lyubov <serdechnaya.lyubov@gmail.com>
2021-04-02 14:34:38 +0300

    commit3

--- js/HW3_task01.js
+++ js/HW3_task01.js
@@ -1,3 +1,4 @@
+//"use strict";
 /*
 Задание 1. Напишите функцию range(), принимающую два аргумента: 
 начало и конец диапазона, и возвращающую массив, который содержит все числа из диапазона, 
@@ -7,3 +8,33 @@
 например, range(5, 2, -1) должна возвращать массив [5, 4, 3, 2].
 
 */
+
+let x1 = prompt("Enter the start of array:", "");
+let x2 = prompt("Enter the end of array:", "");
+let x3 = prompt("Enter the step for array:", "");
+
+/*
+function process(param, aparam, bparam) {
+    if (bparam == undefined) bparam = false;
+    // . . .
+}*/
+function range(start, end, step) {
+    let array;
+    if (step === undefined) {
+        let delta = end - start;
+        for (let i = 0,; i<delta; i++){
+            array[i] += delta;
+        }
+        return array;
+    }
+    // else {
+    //     for (let i = start; i< end; i++) {
+    //         array[i] = 
+
+    //     }
+    // }
+
+   
+}
+
+alert (range(x1, x2));
